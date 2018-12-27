7db7c5b76c48643604b4f6efdefa8d006cc91eff
dongY <ldcpaul@hanmail.net>
2018-12-28 02:13:58 +0900

    enum 을 이용한 연산의 특징 열거 ,enum 을 이용한 switch 효율적 분기

--- Calculator/Calculator/CalculatorModel.swift
+++ Calculator/Calculator/CalculatorModel.swift
@@ -12,19 +12,43 @@ class CalculatorModel{
     
     private var accumulator = 0.0
     
+    /******************************
+     *    딕셔너리
+     *    연산자(문자열) : 열거형 구조
+     ******************************/
+    private var operations : Dictionary<String , Operation > = [
+        "π" : Operation.Constant,
+        "e" : Operation.UnaryOperation,
+        "cos": Operation.UnaryOperation,
+        "√":Operation.UnaryOperation,
+        ]
     
-    private var operations : Dictionary<String , Double > = [
-    "π" : M_PI,
-    "e" : M_E ]
     
+    //======================
+    // 열거형: 단항 이항 상수 연산
+    //======================
+    enum Operation{
+        case  Constant
+        case  UnaryOperation
+        case  BinaryOperation
+        case  Equals
+    }
     
     func setOperand(operand : Double){}
     
+    //==================================================
+    // Swift에서 switch문은 모든 경우를 다 따져줘야함
+    // enum 열거형으로 분기시 enum의 프로퍼티만큼의 case가 있으면 됨
+    //==================================================
     func performOperation(symbol : String){
-        if let constant = operations[symbol]{
-            accumulator = constant
-        }
-        }
+        if let operation = operations[symbol]{
+        switch operation {
+        case .Constant:break
+        case .UnaryOperation:break
+        case .BinaryOperation:break
+        case .Equals:break
+            }}
+    }
     
     
     var result : Double{
