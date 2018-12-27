1af57011b51a7de28a6a6cf1dd3249a2e4a5bc6c
dongY <ldcpaul@hanmail.net>
2018-12-28 00:37:59 +0900

    MVC패턴으로 분리

--- Calculator/Calculator/CalculatorModel.swift
+++ Calculator/Calculator/CalculatorModel.swift
@@ -10,13 +10,25 @@ import Foundation
 
 class CalculatorModel{
     
+    private var accumulator = 0.0
+    
     func setOperand(operand : Double){}
     
-    func performOperation(symbol : String){}
+    func performOperation(symbol : String){
+        switch symbol {
+        case "π":
+            accumulator=Double.pi
+        case "√":
+            accumulator=sqrt(accumulator)
+        default:
+            break
+        }
+        
+    }
     
     var result : Double{
         get{
-            return 0.0
+            return accumulator
             
         }
     }
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
+    
     //==============================================
     //             계산 프로퍼티 ?
     //  + 변수를 호출 , 수정 할때 자동으로 밑의 메소드들이 실행됨
     //  + 그 내에서 다른 프로퍼티들도 접근 가능
     //===============================================
-   private  var displayValue : Double{
+    private  var displayValue : Double{
         get{ // display.text 가 형변환이 불가능할수도있기떄문에 옵셔널강제추출이 필요
             return Double(display.text!)!;
         }
@@ -24,61 +24,57 @@ class ViewController: UIViewController {
             display.text = String(newValue)
         }
     }
-    //==================================
-    //    사용자 입력중인지를 나타내는 플래그
-    //==================================
-   private var userIsInTheMiddleOfTyping = false
+    
+    private var brain = CalculatorModel()
+    
+    //=======================================
+    //    사용자 피연산자 입력중인지를 나타내는 플래그
+    //=======================================
+    private var userIsInTheMiddleOfTypingOperand = false
     
     //===============
     //   입력창 라벨
     //===============
     @IBOutlet private  weak var display: UILabel!
-   
+    
     //===============================
     //    숫자키패드의 입력에 대한 콜백메소드
     //===============================
     @IBAction private  func touchDigit(_ sender: UIButton) {
         
         /************************
-            버튼에서 title 추출
-        *************************/
-      let digit = sender.currentTitle!
-    
+         버튼에서 title 추출
+         *************************/
+        let digit = sender.currentTitle!
+        
         // 라벨에 직접적으로 들어갈 String  : 자동 형변환
-      let textCurrentlyInDisplay = self.display.text!
+        let textCurrentlyInDisplay = self.display.text!
         
         
-        if(userIsInTheMiddleOfTyping){
+        if(userIsInTheMiddleOfTypingOperand){
             display.text = textCurrentlyInDisplay + digit //
         }
         else{
             display.text=digit
         }
-        userIsInTheMiddleOfTyping=true
+        userIsInTheMiddleOfTypingOperand=true
     }
-   //====================================
-   // 연산자 키패드버튼의 엽력에 대한 콜백 메소드
-   //====================================
+    //====================================
+    // 연산자 키패드버튼의 엽력에 대한 콜백 메소드
+    //====================================
     @IBAction private func performOperation(_ sender: UIButton) {
-        
-        userIsInTheMiddleOfTyping=false
-        
+        if(userIsInTheMiddleOfTypingOperand){
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTypingOperand=false
+        }
         // 연산자 키버튼에서 연산자 추출
         if let mathematicalSymbol = sender.currentTitle{
-
-        if(mathematicalSymbol == "𝝿"){
-            displayValue = Double.pi;
+            brain.performOperation(symbol: mathematicalSymbol)
         }
-        else if(mathematicalSymbol == "√")
-            
-        {displayValue = sqrt(displayValue)
-         
-            }
+        displayValue =  brain.result
+        
         
     }
     
     
-}
-
-
 }
