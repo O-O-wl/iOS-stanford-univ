f5f4bf3cee2e6ecc470f462e156e87adcb8a1e12
dongY <ldcpaul@hanmail.net>
2018-12-27 21:43:49 +0900

    MVC 패턴 이해"

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // 사용자 입력중인지를 나타내는 플래그
     var userIsInTheMiddleOfTyping = false
     
+    // 라벨 --
     @IBOutlet weak var display: UILabel!
    
+    // 숫자키패드의 입력에 대한 콜백메소드
     @IBAction func touchDigit(_ sender: UIButton) {
-        var digit = sender.currentTitle!
-         let textCurrentlyInDisplay = self.display.text!
-        if(userIsInTheMiddleOfTyping){
         
-        display.text = String(Int(textCurrentlyInDisplay + digit)!)
+        // 버튼에서 title 추출
+        let digit = sender.currentTitle!
+        
+        // 라벨에 직접적으로 들어갈 String  : 자동 형변환
+        let textCurrentlyInDisplay = self.display.text!
+        
+        
+        if(userIsInTheMiddleOfTyping){
+            display.text = String(Int(textCurrentlyInDisplay + digit)!) //
         }
         else{
             display.text=digit
@@ -27,15 +35,17 @@ class ViewController: UIViewController {
         userIsInTheMiddleOfTyping=true
     }
     
-   
+   // 연산자 키패드버튼의 엽력에 대한 콜백 메소드
     @IBAction func performOperation(_ sender: UIButton) {
+        
         userIsInTheMiddleOfTyping=false
         
+        // 연산자 키버튼에서 연산자 추출
         if let mathematicalSymbol = sender.currentTitle{
 
         if(mathematicalSymbol == "𝝿"){
             display.text = String(Double.pi)}
-        
+            
         
         }
         
