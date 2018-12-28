86d3f863a94d0b0131fbdccf2e29ea3c88b55895
dongY <ldcpaul@hanmail.net>
2018-12-28 23:59:49 +0900

    구조체 생성 , 열거형에 추가

--- Calculator/Calculator/CalculatorModel.swift
+++ Calculator/Calculator/CalculatorModel.swift
@@ -17,10 +17,10 @@ class CalculatorModel{
      *    연산자(문자열) : 열거형 구조
      ******************************/
     private var operations : Dictionary<String , Operation > = [
-        "π" : Operation.Constant,
-        "e" : Operation.UnaryOperation,
-        "cos": Operation.UnaryOperation,
-        "√":Operation.UnaryOperation,
+        "π" : Operation.Constant(Double.pi),
+        "e" : Operation.Constant(M_E),
+        "cos": Operation.UnaryOperation(cos),
+        "√":Operation.UnaryOperation(sqrt),
         ]
     
     
@@ -28,13 +28,16 @@ class CalculatorModel{
     // 열거형: 단항 이항 상수 연산
     //======================
     enum Operation{
-        case  Constant
-        case  UnaryOperation
+        case  Constant(Double)
+        case  UnaryOperation((Double)->Double)
         case  BinaryOperation
         case  Equals
     }
     
-    func setOperand(operand : Double){}
+    func setOperand(operand : Double){
+        accumulator=operand
+        print(cos(90.0))
+    }
     
     //==================================================
     // Swift에서 switch문은 모든 경우를 다 따져줘야함
@@ -42,17 +45,24 @@ class CalculatorModel{
     //==================================================
     func performOperation(symbol : String){
         if let operation = operations[symbol]{
-        switch operation {
-        case .Constant:break
-        case .UnaryOperation:break
-        case .BinaryOperation:break
-        case .Equals:break
+            switch operation {
+            case .Constant(let associatedValue): accumulator = associatedValue
+            case .UnaryOperation(let associateFunction): accumulator = associateFunction(accumulator)
+            case .BinaryOperation:break
+            case .Equals:break
             }}
     }
     
     
+    struct pengingBinaryOperationInfo {
+        var firstOperand : Double
+        func binaryFunction :(Double,Double)->Double
+        
+    }
+    
     var result : Double{
         get{
+            
             return accumulator
             
         }
